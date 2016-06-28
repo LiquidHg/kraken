d27e0a903330c156abf642e6a6c61cf7797c495c
Thomas Carpe <thomas.carpe@liquid-hg.com>
2016-06-28 13:32:26 -0400

    Added missing nuget project for Kraken.SPFarmLibrary

---
+++ Kraken.SPFarmLibrary.Nuget/content/Deploy/Deploy_Dev_Sample.ps1
@@ -0,0 +1,65 @@
+﻿param(
+	[boolean]$FullInstall = $true
+	#TODO your SharePoint server name here
+	, [string]$TargetUrl = "http://yoursharepointservername.local"
+	, [string]$TargetWebApp = "http://yoursharepointservername.local"
+	, [string]$SlnName = ""
+)
+
+#
+# See the following KB article if you see the error below.
+# "The local farm is not accessible. Cmdlets with FeatureDependencyId are not registered."
+# Followed by
+# "Get-SPSolution : Microsoft SharePoint is not supported with version 4.0.x of the Microsoft .Net Runtime."
+# http://support.microsoft.com/kb/2796733
+# and http://blogs.technet.com/b/alexsearch/archive/2013/12/11/sharepoint-2010-powershell-incompatibility-with-net-4-x.aspx
+#
+
+$SPVersion = "2013"
+$SPHiveNum = "15"
+
+# This script is slightly outdated due to changes in the name
+# of the WSP files for 2010 and 2013 versions.
+# So the above changes haven't been tested as of 2016-04-20
+
+$SlnName_Farm = "Kraken.SPFarmLibrary.$SPVersion.wsp"
+$SlnName_Logging = "Kraken.SPLoggingCategories.$SPVersion.wsp"
+
+$SharePointRoot = "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\$SPHiveNum"
+
+Write-Host "Including Fucntion Libraries" -ForegroundColor Green
+. ./SPDeployFunctions.ps1
+Test-SPDeployFunctions
+Add-SPSnapIn
+Start-AdminService
+
+## Not needed the WSP are already in the deploy folder now
+# Write-Host "Copying WSP Packages from Source." -ForegroundColor Green
+# Copy-Item -Path ..\$SlnName -Destination .
+# Copy-Item -Path ..\$SlnName_Farm -Destination .
+# Copy-Item -Path ..\$SlnName_Logging -Destination .
+
+$wspPath = "$(Get-ScriptDirectory)\$SlnName"
+$wspPath_Farm14 = "$(Get-ScriptDirectory)\$SlnName_Farm"
+$wspPath_Logging = "$(Get-ScriptDirectory)\$SlnName_Logging"
+
+Write-Host "Checking Solution Dependencies" -ForegroundColor Green
+RemoveAdd-Solution -SlnName $SlnName_Farm -WspPath $wspPath_Farm14
+RemoveAdd-Solution -SlnName $SlnName_Logging -WspPath $wspPath_Logging
+
+## Handles uninstall (retract), remove (delete), add (upload), and install (deploy)
+## Write-Host "Removing/Adding Solution to Farm" -ForegroundColor Green
+## RemoveAdd-Solution -SlnName $SlnName  -WspPath $wspPath ## -TargetUrl $TargetUrl
+
+if ($FullInstall) {
+	Write-Host "Restarting Timer Job Service" -ForegroundColor Green
+	Restart-Service "SPTimerV4" ## -displayname "SharePoint 2010 Timer"
+	Write-Host "Restarting IIS" -ForegroundColor Green
+	iisreset
+}
+
+Write-Host "Activating Logging Categories..." -ForegroundColor Green
+Activate-SPFeature -FeatureId f0511646-a032-4cfb-8d81-2ecad3548ce3
+
+Write-Host "Done." -ForegroundColor Green
+Read-Host "Press ENTER."
---
+++ Kraken.SPFarmLibrary.Nuget/content/Deploy/SPDeployFunctions.ps1
@@ -0,0 +1,190 @@
+﻿function Test-SPDeployFunctions
+{
+	Write-Host "Successfully included SPDeployFunctions.ps1" -ForegroundCOlor Yellow
+}
+
+function Get-ScriptDirectory
+{
+  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+  Split-Path $Invocation.MyCommand.Path
+}
+
+function Install-SPFarmSolution 
+{
+param($SolutionName, $targetUrl) 
+
+	$Solution = Get-SPSolution | ? { $_.Name -eq $SolutionName }
+	if ($Solution -eq $null) {
+		Write-Host "$SolutionName does not exist." -ForegroundCOlor Yellow
+	} else { if ($Solution.Deployed -eq $true) {
+		Write-Host "$SolutionName already deployed."
+	} else {
+		Write-Host "Deploying $SolutionName with ID $($Solution.ID)"
+		Write-Host "Deploying..." -nonewline
+		if ($targetUrl -eq $null -or [string]::IsNullOrEmpty($targetUrl)) {
+			Install-SPSolution –Identity $Solution.ID –GACDeployment 
+		} else {
+			Install-SPSolution –Identity $Solution.ID -WebApplication $targetUrl –GACDeployment 
+		}
+		while ($Solution.JobExists) {
+			Start-Sleep 2
+			Write-Host "." -nonewline
+		}
+		if ($Solution.Deployed -eq $true) {
+			Write-Host "Success!"
+		} else {
+			Write-Host "Fail!"
+		}
+	}}
+}
+
+function Activate-SPFeature
+{
+param($FeatureID, $targetUrl) 
+	$f = Get-SPFeature | ? { $_.ID -eq $FeatureID }
+	if ($f -eq $null) {
+		Write-Host "Feature with ID $FeatureID does not exist at $targetUrl!" -ForegroundCOlor Yellow
+	} else {
+		switch ($f.Scope) {
+			"Farm" {
+				$sLabel = "Farm"
+				$target = $null
+			}
+			"Site" {
+				$sLabel = "Site Collection"
+				$target = Get-SPSite $targetUrl
+			}
+			default { ## "Web"
+				$sLabel = "Web Site"
+				$target = Get-SPWeb $targetUrl
+			}
+		}
+		if ($target -eq $null -and $f.Scope -ne "Farm") {
+			Write-Host "$($target.Url) does not exist!" -ForegroundCOlor Yellow
+		} else {
+			Write-Host "Activating Feature $($f.DisplayName) for $sLabel $targetUrl" -ForegroundCOlor Green
+			if ($targetUrl -ne $null) {
+				Write-Host "  at $targetUrl" -ForegroundCOlor Green
+			}
+			switch ($f.Scope) {
+				"Farm" {
+					$Feature = Get-SPFeature -Farm | ? { $_.ID -eq $FeatureID }
+				}
+				"Site" {
+					$Feature = Get-SPFeature -Site $target | ? { $_.ID -eq $FeatureID }
+				}
+				default {
+					$Feature = Get-SPFeature -Web $target | ? { $_.ID -eq $FeatureID }
+				}
+			}
+			if ($Feature -ne $null) {
+				Write-Host "Feature was already Activated."
+				Write-Host "De-activating..."
+				if ($target -ne $null) {
+					Disable-SPFeature –Identity $FeatureID –Url $target.Url
+				} else {
+					Disable-SPFeature –Identity $FeatureID
+				}
+			} ## else {
+			Write-Host "Activating..."
+			if ($target -ne $null) {
+				Enable-SPFeature –Identity $FeatureID –Url $target.Url
+			} else {
+				Enable-SPFeature –Identity $FeatureID
+			}
+			Write-Host "Done."
+			##}	
+		}
+	}
+}
+
+## This function helps to remove and re-add a solution on a multi-server farm.
+function RemoveAdd-Solution {
+param(
+	[string]$SlnName,
+	[string]$wspPath,
+	[string]$targetUrl = $null
+)
+	Write-Host "Checking and removing current WSP Package"
+	$sln = Get-SPSolution | ? { $_.Name -eq $SlnName }
+	if ($sln -ne $null) {
+		Write-Host "Removing $SlnName with ID $($sln.Id)"
+		Uninstall-SPSolution -Identity $sln.Id
+
+		Write-Host "Retracting" -nonewline
+		while ($sln.JobExists) {
+			Start-Sleep 2
+			Write-Host "." -nonewline
+		}
+		if ($sln.Deployed -eq $false) {
+			Write-Host "Success!"
+		} else {
+			Write-Host "Fail!" -ForegroundColor Red
+		}
+
+		Write-Host "Removing $SlnName with ID $($sln.Id)"
+		Remove-SPSolution -Identity $sln.Id
+	}
+	Write-Host "Adding solution at $wspPath" -ForegroundColor Green
+	Add-SPSolution $wspPath
+
+	## Deploy (Install) Solution on farm
+	Write-Host "Deploying Solution" -ForegroundColor Green
+	Install-SPFarmSolution -SolutionName $SlnName -TargetUrl $targetUrl ## -AllowGACDeployment
+}
+
+function Register-SPAlerts {
+param(
+	[string]$xmlTemplatePath
+	,[string]$TargetUrl
+	,[int]$minutes
+)
+	# Note there is no powershell equivalent to updatealerttemplates as of 5/12/2010
+	# http://technet.microsoft.com/en-us/library/ff621081.aspx
+	Write-Host "Running Required STSADM Commands" -ForegroundColor Green
+	Write-Host "Updating Alert Templates..."
+	Write-Host "Running command: stsadm -o updatealerttemplates -filename ""$($xmlTemplatePath)"" -url ""$($TargetUrl)"""
+	stsadm -o updatealerttemplates -filename "$($xmlTemplatePath)" -url "$($TargetUrl)"
+
+	Write-Host "Checking to make sure Alerts are enabled."
+	stsadm -o getproperty -url $TargetUrl -pn alerts-enabled
+	Write-Host ""
+	stsadm -o getproperty -url $TargetUrl -pn job-immediate-alerts
+	Write-Host ""
+	##stsadm -o getproperty -url $TargetUrl -pn job-daily-alerts
+	##Write-Host ""
+	##stsadm -o getproperty -url $TargetUrl -pn job-weekly-alerts
+	##Write-Host ""
+
+	Write-Host "Change Alert schedule to 'Every $($minutes) Minutes'"
+	## Change this value to less frequent unless the customer requires otheerwise.
+	stsadm -o setproperty -url $TargetUrl -pn job-immediate-alerts -pv "every $($minutes) minutes between 0 and 59" 
+	## This property is said to have no effect in SP2010.
+	##stsadm -o setproperty -url $TargetUrl -pn job-daily-alerts -pv "daily between 0:01:00 and 23:59:00" 
+
+	Write-Host "Disable and re-enable alerts"
+	stsadm -o setproperty -url $TargetUrl -pn alerts-enabled -pv "false"
+	stsadm -o setproperty -url $TargetUrl -pn alerts-enabled -pv "true"
+}
+
+function Add-SPSnapIn {
+	Write-Host "Add SharePoint PowerShell Extensions (if needed)" -ForegroundColor Green
+	$snap = Get-PSSnapin | ? { $_.Name -eq "Microsoft.SharePoint.PowerShell" }
+	if ($snap -eq $null) {
+		Add-PSSnapin Microsoft.SharePoint.PowerShell
+		Return $true
+	}
+	Return $false
+}
+
+function Start-AdminService {
+	Write-Host "Start Admin Service (if stopped)" -ForegroundColor Green
+	$AdminServiceName = "SPAdminV4"
+	$IsAdminServiceWasRunning = $true;
+	if ($(Get-Service $AdminServiceName).Status -eq "Stopped") {
+		$IsAdminServiceWasRunning = $false;
+		Write-Host 'Starting SharePoint Admin Service...'
+		Start-Service $AdminServiceName
+		Write-Host 'Done.'
+	}
+}
