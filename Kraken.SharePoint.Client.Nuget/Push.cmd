6a348b2fb05a02cfd5e596d317ee0759dcdb3e58
Thomas Carpe <thomas.carpe@liquid-hg.com>
2016-07-26 17:15:09 -0400

    Added Kraken.DotNet.Core nuget project.

---
+++ Kraken.SharePoint.Client.Nuget/Push.cmd
@@ -0,0 +1,6 @@
+﻿REM Update the function below to match the package file name
+nuget push Kraken.SharePoint.Client.0.1.43.symbols.nupkg -Source https://liquidhg.pkgs.visualstudio.com/DefaultCollection/_packaging/Misc/nuget/v2 -ApiKey VSTS
+nuget push Kraken.SharePoint.Client.0.1.43.symbols.nupkg -Source https://nuget.org/api/v2/ -ApiKey 990b2015-0155-4693-a7c6-7e7a7a570c94
+REM Hey, you need get your own upload credentials!
+REM The command to add them will look like this...
+REM nuget sources add -name "Misc" -source https://liquidhg.pkgs.visualstudio.com/DefaultCollection/_packaging/Misc/nuget/v2 -username "thomas.carpe" -password ""
