42ff8569cc2fdc7914d69b4650c9d954a224d0b9
docwattsman <thomas.carpe@liquidmercurysolutions.com>
2016-11-21 17:34:22 -0500

    Added Nuget2 projects to source control.

---
+++ Kraken.SPFarmLibrary.Nuget2/push.cmd
@@ -0,0 +1,3 @@
+nuget push ./bin/Debug45/Kraken.SPFarmLibrary.0.1.2.symbols.nupkg -Source https://liquidhg.pkgs.visualstudio.com/DefaultCollection/_packaging/Misc/nuget/v2 -ApiKey VSTS
+REM Alternative APIKey: yrdvieeozjhl7euk4wnsapqpu3hmnc6gd5jqvbyybvg54qhc7qiq
+pause
