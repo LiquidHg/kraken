6e3796138ec1fb31c3b3c9e77336a6fec7e7a442
docwattsman <thomas.carpe@liquidmercurysolutions.com>
2018-10-18 19:42:04 -0400

    Eliminate pause in Nuget push.cmd files.

--- Kraken.DotNet.Core.Nuget/push.cmd
+++ Kraken.DotNet.Core.Nuget/push.cmd
@@ -2,4 +2,3 @@ REM TODO on new release, update the function below to match the package file nam
 SET NUGET_PUSH_FN=".\bin\Debug45\Kraken.DotNet.Core.0.3.1-alpha05.symbols.nupkg"
 nuget push %NUGET_PUSH_FN% -Source "Misc (private)" -ApiKey VSTS
 nuget push %NUGET_PUSH_FN% -Source https://api.nuget.org/v3/index.json -ApiKey %NUGET_APIKEY_KRAKEN%
-pause
--- Kraken.SharePoint.Apps.Nuget2/Push.cmd
+++ Kraken.SharePoint.Apps.Nuget2/Push.cmd
@@ -2,4 +2,3 @@
 SET NUGET_PUSH_FN=".\bin\Debug\Kraken.SharePoint.Apps.0.3.1-alpha05.symbols.nupkg"
 nuget push %NUGET_PUSH_FN% -Source "Misc (private)" -ApiKey VSTS
 nuget push %NUGET_PUSH_FN% -Source https://api.nuget.org/v3/index.json -ApiKey %NUGET_APIKEY_KRAKEN%
-pause
